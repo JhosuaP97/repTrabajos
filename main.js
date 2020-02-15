0401746504acbe1f9b3a93ca6f183de5075c8b2d
=Jhosua Pachon <jhosuapachon@gmail.com>
2020-02-15 12:15:09 -0500

    Se arregla navegacion

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 $(document).ready(()=>{
     $('ul.o-tabs li a:first').addClass('.o-tabs o-active');
-    $('.o-section article').hide();
-    $('.o-section article:first').show();
-
-    $('ul.o-tabs li a').click((e) =>{
+    $('.o-section div').hide();
+    $('.o-section div:first').show();
+    
+    $('ul.o-tabs li a').click(function(e){
         e.preventDefault();
-        $('ul.o-tabs li a').toggleClass('.o-tabs o-active');
-
-        var activeTab = $(this).prop('href');
-        console.log(activeTab);
-        
+        $('ul.o-tabs li a').removeClass('.o-tabs o-active');
+        $(this).addClass('.o-tabs o-active');
     });
 });
