8f585e198d1857d899f5ddaf9de5bc18c6669fef
=Jhosua Pachon <jhosuapachon@gmail.com>
2020-02-16 22:45:51 -0500

    se hace responsive a 600px,se añade descripciones

--- images.js
+++ images.js
@@ -1,10 +1,13 @@
 let images = ['resources/works/corvette.jpg',
               'resources/works/videoGame-find-a-way.png', 
-              'resources/works/videosInteractivos.PNG'];
+              'resources/works/videosInteractivos.PNG','resources/works/booksearch.jpg'];
 let count = 0;
 
-let titles =['Corvette-Micromachine','Find a Way-Videojuego','Videos interactivos'];
-let descriptions =['description 1','description 2','description 3'];
+let titles =['Corvette-Micromachine','Find a Way-Videojuego','Videos interactivos','App Móvil Booksearch'];
+let descriptions =['Modelo creado en maya 3D para la asignatura de modelado y animación 3D con el objetivo de realizar un video promocional estilo micromachine.',
+'Es un videojuego para PC desarrollado en Unity donde se narra la historia de Orson, quien se encuentra recorriendo una compañía a través de diferentes dimensiones, buscando fragmentos de una materia denominada "Partículas cuánticas" para evitar que la tragedia que lo tiene viajando a través del tiempo y espacio se produzca en primer lugar.',
+'Es un plataforma interactiva desarrollada para la asignatura de Video y Televisión Digital donde se explica por una serie de videos la temática de software las cuáles vienen incluidos 3 diferentes juegos (arrastrar y soltar,ahorcado y completar) '
+,'Aplicación móvil desarrollado en Android Studio, donde consiste en que los estudiantes y profesores de la universidad Autónoma de Occidente pueden reservar libros y revistas indexadas que tenga disponible la biblioteca, además de ver por medio de un mapa la ubicación del texto que desee reservar'];
 
 
 
